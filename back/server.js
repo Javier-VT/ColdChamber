3f0a2920e0cf547dddc624d8d13948d3b8aeb312
Javier VT <geodashdead90099@gmail.com>
2025-11-17 18:24:23 -0600

    Update server.js

--- back/server.js
+++ back/server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json()); // para parsear JSON en el body
+app.use(express.json());
 
 let ultimoDato = { temperatura: null, humedad: null, fecha: null };
 let comandoBuzzer = "OFF";
@@ -14,7 +14,6 @@ app.post('/esp32/data', (req, res) => {
   const { temperatura, humedad } = req.body;
   ultimoDato = { temperatura, humedad, fecha: new Date().toISOString() };
 
-  // lógica para el buzzer
   if (temperatura >= 27) comandoBuzzer = "ON";
   else comandoBuzzer = "OFF";
 
@@ -29,6 +28,7 @@ app.get('/esp32/ultimo', (req, res) => {
   res.json(ultimoDato);
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor corriendo en puerto ${port}`);
 });
+
